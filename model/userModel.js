eb678e4be8f0b74027eb60c108fc4615c8af3f13
Kusum <Kusumlamichhane2015@gmail.com>
2022-04-20 12:13:51 +0545

    Regex added

---
+++ model/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const UserModel= mongoose.model('Login', 
+    mongoose.Schema(
+        {
+            userName: {
+                type: String,
+                required: [true,"Please mention your username"],
+                trim:true
+            },
+            email: {
+                type: String,
+                unique: true,
+                required: [true, "Please add an email"],
+                trim: true,
+                match: [
+                  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                  "Please add a valid email",
+                ], 
+            },
+            
+            password: {
+                type: String,
+                required: true,
+                trim:true
+            }
+        },
+        {timestamps: true}
+    )
+);
+export default UserModel;
