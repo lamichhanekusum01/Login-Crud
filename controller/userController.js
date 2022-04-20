3ad983c995639312e53a561fc5476500068a3859
Kusum <Kusumlamichhane2015@gmail.com>
2022-04-20 11:11:53 +0545

    All User fetches

---
+++ controller/userController.js
@@ -0,0 +1,28 @@
+import UserModel from "../model/userModel.js";
+
+export const allUser = async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        res.json(users);
+    }
+    catch (error) {
+        res.json({ message: "No user found" });
+    }
+};
+
+// export const userLogin = async (req, res) => {
+//     try {
+
+//         const user = await UserModel.find({ $and: [{ email: req.body.email }, { password: req.body.password }] });
+//         if (user) {
+//             res.send("successfull login");
+//         }
+//         else {
+//             throw new error("error");
+//         }
+//         // res.json(user);
+//     }
+//     catch (error) {
+//         res.json({ message: error });
+//     }
+// }
