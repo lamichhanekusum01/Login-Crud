3bbdd9e9c4938fafdbdd44fc1b32901d29a8e30c
Kusum <Kusumlamichhane2015@gmail.com>
2022-04-20 11:08:24 +0545

    Database Connection

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import 'dotenv/config';
 
-const CONN_URI=process.env.CONN_URI
-const db = mongoose.connect(CONN_URI, {
+const CONN_URL=process.env.CONN_URI
+const db = mongoose.connect(CONN_URL, {
     useNewUrlParser: true,
 }).then((response) => {
     console.log('Connected to the database...');
