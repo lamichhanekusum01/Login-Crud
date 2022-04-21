39f1194bbe42ceb49cd2526b76f1f9ab37ce0817
Kusum <Kusumlamichhane2015@gmail.com>
2022-04-21 14:29:09 +0545

    middleware added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-import jsonwebtoken from "jsonwebtoken";
-import { config } from "dotenv";
+// import jsonwebtoken from "jsonwebtoken";
+// import { config } from "dotenv";
 
-const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
-  if (!token) {
-    console.log("i am here");
+// const verifyToken = (req, res, next) => {
+//   const token =
+//     req.body.token || req.query.token || req.headers["x-access-token"];
+//   if (!token) {
+//     console.log("i am here");
 
-    return res.status(401).send("A token is reqiured for authentication");
-  }
-  try {
-    const decoded = jsonwebtoken.verify(token, config.SECRET_KEY);
+//     return res.status(401).send("A token is reqiured for authentication");
+//   }
+//   try {
+//     const decoded = jsonwebtoken.verify(token, config.SECRET_KEY);
 
-    req.userModel = decoded;
-  } catch (error) {
-    return res.status(401).send("Invalid token");
-  }
-  return next();
-};
-export default verifyToken;
+//     req.userModel = decoded;
+//   } catch (error) {
+//     return res.status(401).send("Invalid token");
+//   }
+//   return next();
+// };
+// export default verifyToken;
